a247efe93477f57fe9e0f21c5619e5333c2f1b3b
Daniel Jacobsen <danielgjacobsen@icloud.com>
2018-01-26 14:26:15 -0200

    Retornando para quem chamou
    Retornando para quem chamou

--- insertVal.php
+++ insertVal.php
@@ -5,14 +5,14 @@
 		$login = $_COOKIE['login'];
 			$connect = mysqli_connect('localhost', 'root', '12qwaszx', 'independence');
 			if($investimento == "" || $investimento == null || $perfil == "" || $perfil == null || $valor == "" || $valor == null){
-						echo"<script language='javascript' type='text/javascript'>alert('Todos os campos devem ser preenchidos');</script>";
+						echo"<script language='javascript' type='text/javascript'>alert('Todos os campos devem ser preenchidos');window.location.href=document.referrer</script>";
 							}else{
 										$query = "INSERT INTO valores_diarios (login, investimento, valor) VALUES ('$login','$investimento', '$valor')";
 												$insert = mysqli_query($connect, $query);
 												if($insert){
-																echo"<script language='javascript' type='text/javascript'>alert('Valor cadastrado com sucesso!');</script>";
+																echo"<script language='javascript' type='text/javascript'>alert('Valor cadastrado com sucesso!');window.location.href=document.referrer</script>";
 																		}else{
-																						echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar o valor...houve alguma falha');</script>";
+																						echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar o valor...houve alguma falha');window.location.href=document.referrer</script>";
 																								}
 													}
 ?>
