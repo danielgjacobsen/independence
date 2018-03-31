71f28ce746e4758dd52233540ad4fc5b86be4535
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-03-31 09:33:13 -0300

    add diff no grafico

--- js/line-db-TesouroIpca2024NtnbPrinc-php.js
+++ js/line-db-TesouroIpca2024NtnbPrinc-php.js
@@ -6,15 +6,19 @@ $(document).ready(function(){
 			console.log(data);
 			var value = {
 				dia : [],
-				TESOURO_IPCA_2024_NTNB_PRINC : [],
+				TESOURO_IPCA_2024_NTNB_PRINC : [],,
+				diff : [],
 			};
 			
 			var len = data.length;
+			var oldValue = 0;
 
 			for(var i = 0; i < len; i++) {
 				if (data[i].investimento == "TESOURO IPCA+ 2024 (NTNB PRINC)"){
 					value.dia.push(data[i].data);
 					value.TESOURO_IPCA_2024_NTNB_PRINC.push(data[i].valor);
+					value.diff.push(data[i].valor - oldValue);
+					oldValue = data[i].valor;
 				}
 			}
 			
@@ -29,6 +33,15 @@ $(document).ready(function(){
 						label : "TESOURO IPCA+ 2024 (NTNB PRINC)",
 						data : value.TESOURO_IPCA_2024_NTNB_PRINC,
 						backgroundColor : "blue",
+						borderColor : "lightblue",
+						fill : false,
+						lineTension : 0,
+						pointRadius : 5
+					},
+					{
+						label : "Diferenca",
+						data : value.diff,
+						backgroundColor : "green",
 						borderColor : "lightblue",
 						fill : false,
 						lineTension : 0,
