0668dbc71aeb7f225be00857d7d128f56475fde8
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-03-31 08:54:50 -0300

    criação de grafico para apresentação da evolução em reais

---
+++ js/line-db-previdencia-dif-php.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+	$.ajax({
+		url: "http://35.224.227.85/independence/api/data.php",
+		method: "GET",
+		success: function(data) {
+			console.log(data);
+			var value = {
+				dia : [],
+				SICREDI_PREVIDENCIA : [],
+			};
+			
+			var len = data.length;
+
+			for(var i = 0; i < len; i++) {
+				if (data[i].investimento == "SICREDI PREVIDENCIA"){
+					value.dia.push(data[i].data);
+					value.SICREDI_PREVIDENCIA.push(data[i].valor);
+				}
+			}
+			
+			console.log(value);
+			
+			var ctx = $("#myChart");
+			
+			var data = {
+				labels : value.dia,
+				datasets : [
+					{
+						label : "SICREDI PREVIDENCIA",
+						data : value.SICREDI_PREVIDENCIA,
+						backgroundColor : "blue",
+						borderColor : "lightblue",
+						fill : false,
+						lineTension : 0,
+						pointRadius : 5
+					}
+				]
+			};
+			
+			var myChart = new Chart( ctx, {
+				type : "line",
+				data : data,
+				options : {}
+			});
+			
+		},
+		error: function(data) {
+			console.log(data);
+		}
+	});
+});
